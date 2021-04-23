34e3969b5e994ed201e3e48a11095fc9d92dd8c3
Vedpathak <sumit.vedpathak@bell.corp.bce.ca>
2021-04-23 16:37:06 -0400

    Added Constants

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
+const Constants = require('./constants')
 
 const app = express()
 
-const port = 3000
-app.listen(port, () => {
-    console.log('Listening to port-'+port)
+app.listen(Constants.PORT, () => {
+    console.log('Listening to port - ' + Constants.PORT)
 })
